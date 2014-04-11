b93afff9d9b3b513167c5c54dc577f969f2c1da6
Ted Ying <yingted@gmail.com>
2014-04-11 15:12:43 -0400

    fix bfs

--- power-test.cpp
+++ power-test.cpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <vector>
 #include <sstream>
+#include <queue>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 
@@ -54,40 +55,50 @@ struct rand_bool{
 
 const Iso_cuboid_3 domain(-1, -1, -1, 1, 1, 1);
 double branch_radius_cutoff;
+std::map<const char*, int>hit_counter;
 
-void rand_pts(std::set<Weighted_point>& pts, const Point c, const double r, rand_bool& rng){
-	if (r <= branch_radius_cutoff)
-		goto leaf;
-	{
-		const Weight parent_weight((2*r)*(2*r));
-		for (int i = -3; i <= 3; i += 2)
-			for (int j = -3; j <= 3; j += 2)
-				for (int k = -3; k <= 3; k += 2)
-					if (i*i+j*j+k*k == 11 && pts.count(Weighted_point(c+r*Vector_3(i, j, k), parent_weight)))
-						goto leaf;
-	}
-	if (!rng()){
+void rand_pts(std::set<Weighted_point>& pts, Point c, double r, rand_bool& rng){
+	std::queue<std::pair<Point, double> >q;
+	q.push(std::make_pair(c, r));
+	while (!q.empty()){
+		c = q.front().first;
+		r = q.front().second;
+		q.pop();
+		if (r <= branch_radius_cutoff)
+			goto leaf;
+		{
+			const Weight parent_weight((2*r)*(2*r));
+			for (int i = -3; i <= 3; i += 2)
+				for (int j = -3; j <= 3; j += 2)
+					for (int k = -3; k <= 3; k += 2)
+						if (i*i+j*j+k*k == 11){
+							const Point p = c+r*Vector_3(i, j, k);
+							if (!domain.has_on_unbounded_side(p) && pts.count(Weighted_point(p, parent_weight)))
+								goto leaf;
+						}
+		}
+		if (!rng()){
 leaf:
-		const Weight w(r*r);
-		pts.insert(Weighted_point(c, w));
-		for (int i = -1; i <= 1; ++i) // insert ghost cells to check boundary conditions
-			for (int j = -1; j <= 1; ++j)
-				for (int k = -1; k <= 1; ++k)
-					if (i*i+j*j+k*k == 1){
-						const Point p = c+(2*r)*Vector_3(i, j, k);
-						if (domain.has_on_unbounded_side(p))
-							pts.insert(Weighted_point(p, w)); // ghost cell outside
-					}
-		return;
+			const Weight w(r*r);
+			pts.insert(Weighted_point(c, w));
+			for (int i = -1; i <= 1; ++i) // insert ghost cells to check boundary conditions
+				for (int j = -1; j <= 1; ++j)
+					for (int k = -1; k <= 1; ++k)
+						if (i*i+j*j+k*k == 1){
+							const Point p = c+(2*r)*Vector_3(i, j, k);
+							if (domain.has_on_unbounded_side(p))
+								pts.insert(Weighted_point(p, w)); // ghost cell outside
+						}
+			continue;
+		}
+		const double child_r = .5*r;
+		for (int i = -1; i <= 1; i += 2)
+			for (int j = -1; j <= 1; j += 2)
+				for (int k = -1; k <= 1; k += 2)
+					q.push(std::make_pair(c+child_r*Vector_3(i, j, k), child_r));
 	}
-	const double child_r = .5*r;
-	for (int i = -1; i <= 1; i += 2)
-		for (int j = -1; j <= 1; j += 2)
-			for (int k = -1; k <= 1; k += 2)
-				rand_pts(pts, c+child_r*Vector_3(i, j, k), child_r, rng);
 }
 
-int tested_count;
 time_t start, last;
 
 bool test_octree(rand_bool& rng){
@@ -96,6 +107,15 @@ bool test_octree(rand_bool& rng){
 	//std::cout << "cells (including ghost): " << pts.size() << std::endl;
 	//for (std::set<Weighted_point>::const_iterator it = pts.begin(); it != pts.end(); ++it)
 	//	std::cout << *it << std::endl;
+	{
+		std::map<Weight, int>weight_count;
+		for (std::set<Weighted_point>::const_iterator it = pts.begin(); it != pts.end(); ++it)
+			++weight_count[it->weight()];
+		for (std::map<Weight, int>::const_iterator it = weight_count.begin(); it != weight_count.end(); ++it)
+			std::cout << it->first << ": " << it->second << std::endl;
+		std::cout << "total: " << pts.size() << std::endl;
+		std::cout << std::endl;
+	}
 	Rt T;
 	ptrdiff_t num_inserted = T.insert(pts.begin(), pts.end());
 	assert(pts.size() == num_inserted); // check for hidden
@@ -104,8 +124,7 @@ bool test_octree(rand_bool& rng){
 	for (Finite_edges_iterator it = T.finite_edges_begin(); it != T.finite_edges_end(); ++it){
 		Weighted_point a = it->first->vertex(it->second)->point(),
 		               b = it->first->vertex(it->third)->point();
-		//if (domain.has_on_unbounded_side(a) && domain.has_on_unbounded_side(b))
-		if (domain.has_on_unbounded_side(a) || domain.has_on_unbounded_side(b)) // XXX should check ghost
+		if (domain.has_on_unbounded_side(a) && domain.has_on_unbounded_side(b))
 			continue;
 		if (a.weight() < b.weight())
 			std::swap(a, b); // make a larger
@@ -120,26 +139,27 @@ bool test_octree(rand_bool& rng){
 			case 0: // 3d intersection volume
 				assert(false);
 			case 1: // 2d plane
-				if (R2 == r2) // case 1: same level
+				if (R2 == r2){
+					++hit_counter["case 1: same level"];
 					assert(COUNT(0) == 2);
-				else if (R2 == 4*r2) // case 2: unit "knight's move"
+				}else if (R2 == 4*r2){
+					++hit_counter["case 2: unit \"knight's move\""];
 					assert(COUNT(r2) == 2);
-				else
+				}else
 					assert(false);
 				break;
 			case 2: // 1d (small slice)
-				if (R2 == 4*r2){ // case 3: abbb
+				if (R2 == 4*r2){
+					++hit_counter["case 3: abbb"];
 					assert(COUNT(r2) == 1);
-					//std::cout << a << ", " << b << std::endl;
-					//if (d[0]*d[0] == rR*rR)
-					//	std::cout << Weighted_point(a+Vector_3(d[0]-copysign(R, d[0]), d[1], d[2]), r2) << std::endl;
 					if (d[0]*d[0] == rR*rR)
 						assert(pts.count(Weighted_point(a+Vector_3(d[0]-copysign(R, d[0]), d[1], d[2]), r2)));
 					if (d[1]*d[1] == rR*rR)
 						assert(pts.count(Weighted_point(a+Vector_3(d[0], d[1]-copysign(R, d[1]), d[2]), r2)));
 					if (d[2]*d[2] == rR*rR)
 						assert(pts.count(Weighted_point(a+Vector_3(d[0], d[1], d[2]-copysign(R, d[2])), r2)));
-				}else if (R2 == r2){ // case 4: axab
+				}else if (R2 == r2){
+					++hit_counter["case 4: axab"];
 					assert(COUNT(0) == 1);
 					//std::cout << a << ", " << b << std::endl;
 					int adjacent_large =
@@ -150,7 +170,7 @@ bool test_octree(rand_bool& rng){
 						+ (d[0] && pts.count(Weighted_point(a+Vector_3(d[0]-copysign(.5*R, d[0]), d[1], d[2]), .25*R2)))
 						+ (d[1] && pts.count(Weighted_point(a+Vector_3(d[0], d[1]-copysign(.5*R, d[1]), d[2]), .25*R2)))
 						+ (d[2] && pts.count(Weighted_point(a+Vector_3(d[0], d[1], d[2]-copysign(.5*R, d[2])), .25*R2)));
-					assert(adjacent_small); // XXX fails
+					assert(adjacent_small);
 					assert(adjacent_large+adjacent_large == 2);
 				}else
 					assert(false);
@@ -161,7 +181,7 @@ bool test_octree(rand_bool& rng){
 		}
 #undef COUNT
 	}
-	++tested_count;
+	const int tested_count = ++hit_counter["octrees"];
 
 	{	
 		const static int period = 5;
@@ -188,6 +208,8 @@ int main(int argc, char *argv[]){
 	std::cout << "testing with branch node cutoff radius " << branch_radius_cutoff << std::endl;
 	start = last = time(NULL);
 	rand_bool().feed(test_octree);
-	std::cout << "finished " << tested_count << " octrees in " << time(NULL)-start << " s" << std::endl;
+	std::cout << "finished in " << time(NULL)-start << " s" << std::endl;
+	for (std::map<const char*, int>::const_iterator it = hit_counter.begin(); it != hit_counter.end(); ++it)
+		printf("%10d %s\n", it->second, it->first);
 	return 0;
 }
